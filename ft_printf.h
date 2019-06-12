ec21c1a8e57d9e3b8739c1d8dba28db4f07dd0f2
Anas  Rchid <archid-@e2r2p14.1337.ma>
2019-06-12 03:53:48 +0100

    Initial Commit

---
+++ ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: archid- <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/12 02:58:15 by archid-           #+#    #+#             */
+/*   Updated: 2019/06/12 03:10:34 by archid-          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+#define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <stdarg.h>
+
+# include "libft/libft.h"
+
+int ft_printf(const char *fmt, ...);
+
+#endif /* FT_PRINTF_H */
